b3ea69dcbc207c4831b535fa55c1c1be74a24c8f
martsos <martinsoos1001@gmail.com>
2026-05-08 10:23:39 +0200

    add dotenv - secure database credentials

--- DEBARRO-PYTHON/database.py
+++ DEBARRO-PYTHON/database.py
@@ -1,9 +1,13 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Kacsa1001",
-        database="debarro_uzemanyag"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
